00f7c2d11fa3930a6670fba419bb9884c6f8646d
Dirk Kretschmann <kretschmanndi@gmail.com>
2020-08-28 00:20:27 +0200

    added basic html canvas

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
-function restartGame(x) {
-    console.log(x);
+function redraw({ gameCanvas }) {
+    console.log('redraw');
+
+    var ctx = gameCanvas.getContext("2d");
+    ctx.moveTo(0,0);
+    ctx.lineTo(200,100);
+    ctx.stroke();
 }
 
-window.restartGame = restartGame;
+function resizeCanvas() {
+    const gameCanvas = document.getElementById("gameCanvas");
 
+    gameCanvas.width = 0.8 * window.innerWidth;
+    gameCanvas.height = 0.8 * window.innerHeight;
+
+    redraw({ gameCanvas });
+}
+
+function restartGame(x) {
+    console.log(x);
+
+    resizeCanvas();
+}
+
+window.addEventListener('resize', resizeCanvas, false);
 window.onload = () => restartGame('game started');
+window.restartGame = restartGame;
